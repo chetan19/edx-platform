93b0c63fd5dd1a353be0807224712e5df26cf5cb
Renzo Lucioni <renzo@renzolucioni.com>
2014-12-16 17:28:30 -0500

    Track verify now and verify later from payment confirmation

--- lms/static/js/verify_student/views/payment_confirmation_step_view.js
+++ lms/static/js/verify_student/views/payment_confirmation_step_view.js
@@ -13,6 +13,22 @@ var edx = edx || {};
     // flag set.  This allows the user to navigate back to the confirmation
     // if he/she wants to.
     // For this reason, we don't need any custom click handlers here.
-    edx.verify_student.PaymentConfirmationStepView = edx.verify_student.StepView.extend({});
+    edx.verify_student.PaymentConfirmationStepView = edx.verify_student.StepView.extend({
+        postRender: function() {
+            // Track clicks on "Verify Now" button
+            $( "#verify_now_button" ).on( 'click', function() {
+                window.analytics.track( 'edx.bi.user.verify.now', {
+                    category: 'verification'
+                });
+            });
+
+            // Track clicks on "Verify Later" button
+            $( "#verify_later_button" ).on( 'click', function() {
+                window.analytics.track( 'edx.bi.user.verify.later', {
+                    category: 'verification'
+                });
+            });
+        }
+    });
 
 })( jQuery );
